eb3466785d2cb1137293882eac654de19f1e3edd
Ilia Shakitko <ilia.shakitko@accenture.com>
2016-01-26 00:25:45 +0100

    Some simple animation added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-   // place your javascripts here
+   // place your javascripts 
+
+   $("a.btn").click(function(){
+	    var block = $( "#block" );
+
+	    block.html('Hello!');
+	    block.animate({
+		    width: "70%",
+		    opacity: 0.4,
+		    marginLeft: "0.6in",
+		    fontSize: "3em",
+		    borderWidth: "10px"
+		}, 1500 );
+	});
 });
